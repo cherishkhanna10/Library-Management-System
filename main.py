0e3c1b340e0adba928f03e078e078844facd97af
chiranjeev khanna <cherishkhanna10@gmail.com>
2026-04-08 22:15:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ def menu():
     print("2. Issue Book")
     print("3. Return Book")
     print("4. Exit")
+list1=["om","elon","hr"]
+def add_element():
+    while True:
+        add=input("enter book to be added ::")
+        if add in list1:
+            print("Book already exit in library")
+        else:
+            list1.append(add)
+            print("Book added in library ::",list1)
+        choice=input("Do you want to continue(yes/no)??::")
+        if choice=="yes":
+            continue
+        else:
+            print("Thank you for visit ")
+            break          
 while True:
     menu()
     choice=input("enter a number as per choice ::")
     if choice == "1":
+        add_element()
         print("Add Book selected")
 
     elif choice == "2":
